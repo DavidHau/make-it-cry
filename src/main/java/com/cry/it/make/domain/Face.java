abcbb8782979a04886cfd868717c73d82bd941d1
David Hau <davidphiliphk@gmail.com>
2018-05-30 21:22:37 +0800

    add static method test case

--- src/main/java/com/cry/it/make/domain/Face.java
+++ src/main/java/com/cry/it/make/domain/Face.java
@@ -2,6 +2,10 @@ package com.cry.it.make.domain;
 
 public class Face {
 
+    public static String getName() {
+        return "haha";
+    }
+
     public String face() {
         return "cry";
     }
--- src/test/java/com/cry/it/make/domain/FaceTest.java
+++ src/test/java/com/cry/it/make/domain/FaceTest.java
@@ -1,12 +1,24 @@
 package com.cry.it.make.domain;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
+import static org.powermock.api.support.membermodification.MemberModifier.stub;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Face.class})
 public class FaceTest {
+    @Before
+    public void setup() {
+        stub(method(Face.class, "getName")).toReturn("asd");
+    }
 
     @Test
     public void should_smile() throws Exception {
@@ -20,5 +32,10 @@ public class FaceTest {
     @Test(timeout = 10)
     public void should_cry() throws Exception {
         assertThat(new Face().face(), is("cry"));
+    }
+
+    @Test
+    public void should_static_method_modified() throws Exception {
+        assertThat(Face.getName(), is("asd"));
     }
 }
