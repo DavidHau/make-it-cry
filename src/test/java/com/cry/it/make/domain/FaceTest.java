93ca4de38814e1c71552262c69fa5abab07d4f35
David Hau <davidphiliphk@gmail.com>
2018-05-30 19:40:08 +0800

    Mock test data and test timeout

--- src/test/java/com/cry/it/make/domain/FaceTest.java
+++ src/test/java/com/cry/it/make/domain/FaceTest.java
@@ -4,15 +4,20 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
 public class FaceTest {
 
-    //@Test
+    @Test
     public void should_smile() throws Exception {
-        assertThat(new Face().face(), is("smile"));
+//        assertThat(new Face().face(), is("smile"));
+        Face face = mock(Face.class);
+        when(face.face()).thenReturn("smile");
+
+        assertThat(face.face(), is("smile"));
     }
 
-    @Test
+    @Test(timeout = 10)
     public void should_cry() throws Exception {
         assertThat(new Face().face(), is("cry"));
     }
